3aede922e57ea001617a96751370bc48ba8e3c13
Buivanbi24 <bibuivan1@gmail.com>
2024-06-27 18:27:40 +0700

    khoi tao

---
+++ assets/script/script.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    //aos git
+    AOS.init({
+        once: false,
+    });
+    //scroll to top
+    var $button = $.backToTop({
+        backgroundColor: '#3c220b', color: '#ede0d0',
+
+    });
+});
